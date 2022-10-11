ef7b68c9b9316882d18c628b116f8093d76a36ee
Christian Mogensen <Christianm@superoffice.com>
2022-10-11 11:41:09 +0200

    Robuster

--- build/Copy-Changed-Files-To-Translations.ps1
+++ build/Copy-Changed-Files-To-Translations.ps1
@@ -7,20 +7,27 @@ $log = &{ git log --since="$sinceWhen" --name-only }
 $changes = @($log) -match "user-guide/en/a*" | Select-Object -unique | Where-Object { Test-Path $_ }
 Write-Output "Changes"
 Write-Output $changes
+Write-Output "-----"
+Get-ChildItem *.zip
+Write-Output "Creating archive '$name'"
 Compress-Archive -DestinationPath "translate-$name-en-changes.zip" -Path $changes
 Get-ChildItem *.zip
 #
+Write-Output "Copying EN to languages"
 $langs = @( 'no', 'de', 'sv' )
 $sourceFolderName = "user-guide/en/"
 foreach ($lang in $langs)
 {
+  Write-Output "Copying en to $lang"
   $targetFolderName = "user-guide/$lang/"
   $targetLanguage = "language: $lang"
   foreach ($itemToCopy in $changes)
   {
-      $targetPathAndFile =  $itemToCopy.Replace( $sourceFolderName , $targetFolderName )
-      $targetfolder = Split-Path $targetPathAndFile -Parent
+    $targetPathAndFile =  $itemToCopy.Replace( $sourceFolderName , $targetFolderName )
+    $targetfolder = Split-Path $targetPathAndFile -Parent
 
+    if( Test-Path $itemToCopy )
+    {
       # If destination folder doesn't exist
       if (!(Test-Path $targetfolder -PathType Container)) {
           #Create destination folder
@@ -37,6 +44,7 @@ foreach ($lang in $langs)
         $newcontent = $content -replace "(?ms)^language: *en *", $targetLanguage
         Set-Content  -path $targetPathAndFile $newcontent
       }
+    }
   }
 }
 echo "Done"
