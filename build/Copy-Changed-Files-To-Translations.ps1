7aaa6c833dd233b60addba4122b95ef4c136646a
Christian Mogensen <Christianm@superoffice.com>
2022-10-14 11:59:35 +0200

    New, working translation workflow

--- build/Copy-Changed-Files-To-Translations.ps1
+++ build/Copy-Changed-Files-To-Translations.ps1
@@ -1,27 +1,56 @@
-param ($name, $sinceWhen)
+param ($name, $sinceWhen, $basedir)
 
-Write-Output "Current loc"
-Get-Location
+$sourceFolderName = "user-guide/en/"
+
+Write-Output "basedir=$basedir"
+Set-Location $basedir
 
 $log = &{ git log --since="$sinceWhen" --name-only }
-$changes = @($log) -match "user-guide/en/a*" | Select-Object -unique
-Compress-Archive -DestinationPath "translate-$name-en-changes.zip" -Path $changes
-Get-Location
-Get-ChildItem *.zip
+$changes = @($log) -like "user-guide/en/a*" | Select-Object -unique | Where-Object { Test-Path -Path $_ -PathType Leaf}
 Write-Output "Changes"
-Write-Output $changes
+$targetFolderName = "en-changes/"
+New-Item -Path $targetFolderName -ItemType Directory -Force | Out-Null
+foreach ($itemToCopy in $changes)
+{
+  if( Test-Path -Path $itemToCopy )
+  {
+    $targetPathAndFile =  $itemToCopy.Replace( $sourceFolderName , $targetFolderName )
+    $targetfolder = Split-Path $targetPathAndFile -Parent
+
+    if( Test-Path $itemToCopy )
+    {
+      # If destination folder doesn't exist
+      if (!(Test-Path $targetfolder -PathType Container)) {
+          # Create destination folder
+          New-Item -Path $targetfolder -ItemType Directory -Force | Out-Null
+      }
+
+      Copy-Item -Path $itemToCopy -Destination $targetPathAndFile 
+      Write-Output $targetPathAndFile
+    }
+  }
+}
+Write-Output "-----"
+
+Write-Output "Creating archive '$name'"
+Compress-Archive -DestinationPath "translate-$name-en-changes.zip" -Path $targetFolderName
+Get-ChildItem *.zip
+Remove-Item -Path $targetFolderName -Recurse -Force 
 #
+Write-Output "Copying EN to languages folders"
 $langs = @( 'no', 'de', 'sv' )
-$sourceFolderName = "user-guide/en/"
 foreach ($lang in $langs)
 {
+  Write-Output "Copying en to $lang"
   $targetFolderName = "user-guide/$lang/"
   $targetLanguage = "language: $lang"
   foreach ($itemToCopy in $changes)
   {
-      $targetPathAndFile =  $itemToCopy.Replace( $sourceFolderName , $targetFolderName )
-      $targetfolder = Split-Path $targetPathAndFile -Parent
+    $targetPathAndFile =  $itemToCopy.Replace( $sourceFolderName , $targetFolderName )
+    $targetfolder = Split-Path $targetPathAndFile -Parent
 
+    if( Test-Path $itemToCopy )
+    {
       # If destination folder doesn't exist
       if (!(Test-Path $targetfolder -PathType Container)) {
           #Create destination folder
@@ -31,13 +60,15 @@ foreach ($lang in $langs)
       Copy-Item -Path $itemToCopy -Destination $targetPathAndFile 
       if( $itemToCopy -like "*.md")
       {
-        echo "$itemToCopy --> $targetPathAndFile"
+        # Write-Output "$itemToCopy --> $targetPathAndFile"
         # Update language code
         $content = Get-Content -path $targetPathAndFile -raw # get as single string
         # (?m) = regex mode select multiline line matching
         $newcontent = $content -replace "(?ms)^language: *en *", $targetLanguage
         Set-Content  -path $targetPathAndFile $newcontent
       }
+    }
   }
 }
-echo "Done"
+Write-Output "Done copying files"
+ 
