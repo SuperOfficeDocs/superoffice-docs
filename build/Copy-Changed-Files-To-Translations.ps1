b58d2eaf576fdaf9f1ca8a96712454bf7d6b5f6c
Tony Yates <tony@anthonyyates.com>
2022-12-19 09:15:41 +0100

    Update build zip script

--- build/Copy-Changed-Files-To-Translations.ps1
+++ build/Copy-Changed-Files-To-Translations.ps1
@@ -1,12 +1,12 @@
 param ($name, $sinceWhen, $basedir)
 
-$sourceFolderName = "user-guide/en/"
+$sourceFolderName = "docs/en/"
 
 Write-Output "basedir=$basedir"
 Set-Location $basedir
 
 $log = &{ git log --since="$sinceWhen" --name-only }
-$changes = @($log) -like "user-guide/en/a*" | Select-Object -unique | Where-Object { Test-Path -Path $_ -PathType Leaf}
+$changes = @($log) -like "*/learn/**" | Select-Object -unique | Where-Object { Test-Path -Path $_ -PathType Leaf}
 Write-Output "Changes"
 $targetFolderName = "en-changes/"
 New-Item -Path $targetFolderName -ItemType Directory -Force | Out-Null
@@ -32,7 +32,7 @@ foreach ($itemToCopy in $changes)
 }
 Write-Output "-----"
 
-Write-Output "Creating archive '$name'"
+Write-Output "Creating archive '$name' in '$targetFolderName'"
 Compress-Archive -DestinationPath "translate-$name-en-changes.zip" -Path $targetFolderName
 Get-ChildItem *.zip
 Remove-Item -Path $targetFolderName -Recurse -Force 
@@ -42,7 +42,7 @@ $langs = @( 'no', 'de', 'sv' )
 foreach ($lang in $langs)
 {
   Write-Output "Copying en to $lang"
-  $targetFolderName = "user-guide/$lang/"
+  $targetFolderName = "docs/$lang/"
   $targetLanguage = "language: $lang"
   foreach ($itemToCopy in $changes)
   {
@@ -58,7 +58,8 @@ foreach ($lang in $langs)
       }
 
       Copy-Item -Path $itemToCopy -Destination $targetPathAndFile 
-      if( $itemToCopy -like "*.md")
+
+      if( $itemToCopy -like "*.md" )
       {
         # Write-Output "$itemToCopy --> $targetPathAndFile"
         # Update language code
