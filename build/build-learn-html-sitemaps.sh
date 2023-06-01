24e32cf15f00c496e2cd52c274f06be1aed171c8
Christian Mogensen <Christianm@superoffice.com>
2023-06-01 10:37:52 +0200

    Adding learn-xx.html sitemap generation to build pipeline.

---
+++ build/build-learn-html-sitemaps.sh
@@ -0,0 +1,23 @@
+# 
+# Generate HTML sitemaps from sitemap.xml
+
+pwd
+cd _site
+ls
+
+echo "<title>Learn english</title>" > learn-en.html
+echo "<title>Learn norsk</title>" > learn-no.html
+echo "<title>Learn svensk</title>" > learn-sv.html
+echo "<title>Learn dansk</title>" > learn-da.html
+echo "<title>Learn dutch</title>" > learn-nl.html
+echo "<title>Learn german</title>" > learn-de.html
+
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/en/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-en.html
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/no/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-no.html
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/sv/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-sv.html
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/da/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-da.html
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/nl/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-nl.html
+cat sitemap.xml | grep "loc" | grep "/learn/" | grep "/de/" | sed -e 's|<loc>\([^<]*\)</loc>|<a href="\1">\1</a><br>|g' >> learn-de.html
+
+ls -l learn*.html
+cd ..
